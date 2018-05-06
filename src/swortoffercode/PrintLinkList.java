931ceaa401461f2dd5cf34a4d11df5b8f74eb724
Rain <l>
2018-05-06 16:45:24 +0800

    3、从尾到头打印链表

---
+++ src/swortoffercode/PrintLinkList.java
@@ -0,0 +1,40 @@
+package swortoffercode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * 从头到尾打印链表
+ * 输入一个链表，从尾到头打印链表每个节点的值。
+ *
+ * 借助栈
+ * @author LiaoYangJun
+ * @createTime 2018/5/6.
+ */
+public class PrintLinkList {
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!stack.empty()) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
+
